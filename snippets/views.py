8622f7e6888aa53f523f0a23de62e057367d291f
Dipen Parihar <parihar.dipen@gmail.com>
2024-02-28 18:57:54 -0500

    Add snippet application

---
+++ snippets/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Snippet
+from .serializers import SnippetSerializer
+
+def snippets(request):
+    return HttpResponse('Hello all')
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
